47d3ea9170cdb1b43a2eb86970d1ae07d7c906cd
sikethedev <ivoinestrachan242@gmail.com>
2021-08-13 15:50:19 +0900

    location fix

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./contact.module.css";
-import Img from "../../assets/formImg.png";
+import location from "../../assets/location.png";
 import { BsArrowUpRight } from "react-icons/bs";
 import Navbar from "../Navbar";
 
@@ -24,7 +24,11 @@ const Contact = () => {
           </form>
         </div>
         <div className={styles.imgContainer}>
-          <img src={Img} alt="form img" className={styles.img} />
+          <a
+          href="https://www.google.com/maps/dir/25.0652528,-77.3441077/windsor+albany+location/@25.0330856,-77.4901167,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x892f66c04cb4e63f:0xdb28c3c1c2e1a1fb!2m2!1d-77.4960708!2d25.0159378"
+          >
+          <img src={location} alt="locaton" className={styles.img} />
+          </a>
         </div>
       </div>
     </>
