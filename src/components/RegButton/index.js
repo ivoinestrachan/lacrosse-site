d19b321076ecade37a2f22f9e543fd68bf431302
Manish674 <manishchoudhary4905@gmail.com>
2021-08-15 07:54:47 +0530

    Linked the button

--- src/components/RegButton/index.js
+++ src/components/RegButton/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsArrowUpRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import styles from "./regbutton.module.css";
 
 const index = () => {
@@ -15,13 +16,19 @@ const index = () => {
             <h4 className={styles.buttonText}>
               You're just a click away, click 👇
             </h4>
-            <div className={styles.buttonWrapper}>
-              <button className={styles.button}>Register</button>
 
-              <div className={styles.iconWrapper}>
-                <BsArrowUpRight className={styles.icon} />
+            <Link
+              to="/register"
+              style={{ textDecoration: "none", cursor: "pointer" }}
+            >
+              <div className={styles.buttonWrapper}>
+                <button className={styles.button}>Register</button>
+
+                <div className={styles.iconWrapper}>
+                  <BsArrowUpRight className={styles.icon} />
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
